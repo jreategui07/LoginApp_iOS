447568018f44f9b6f643cf1d567372018598f1bc
Jonathan Reátegui <j.reategui2012@gmail.com>
2025-01-10 10:24:00 -0500

    Adding empty half-sheet

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -24,6 +24,17 @@ class ViewController: UIViewController {
         return imageView
     }()
     
+    private let halfSheetView: UIView = {
+        let view = UIView()
+        view.backgroundColor = UIColor.systemBackground
+        view.layer.cornerRadius = 20
+        view.layer.shadowColor = UIColor.black.cgColor
+        view.layer.shadowOpacity = 0.2
+        view.layer.shadowRadius = 10
+        view.translatesAutoresizingMaskIntoConstraints = false
+        return view
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -31,10 +42,19 @@ class ViewController: UIViewController {
         self.view.addSubview(logoImageView)
         
         NSLayoutConstraint.activate([
-            logoImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            logoImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 40),
             logoImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
             logoImageView.widthAnchor.constraint(equalToConstant: 140),
             logoImageView.heightAnchor.constraint(equalToConstant: 140)
+        ])
+        
+        self.view.addSubview(halfSheetView)
+
+        NSLayoutConstraint.activate([
+            halfSheetView.leadingAnchor.constraint(equalTo: self.view.leadingAnchor),
+            halfSheetView.trailingAnchor.constraint(equalTo: self.view.trailingAnchor),
+            halfSheetView.bottomAnchor.constraint(equalTo: self.view.bottomAnchor),
+            halfSheetView.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.5)
         ])
     }
 }
