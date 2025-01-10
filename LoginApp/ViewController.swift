f641f3b980a474340a85c68eb31b23a016e7c0ae
Jonathan Reátegui <j.reategui2012@gmail.com>
2025-01-10 01:47:58 -0500

    Initial commit

---
+++ LoginApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LoginApp
+//
+//  Created by Jonathan Reátegui on 2025-01-10.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
