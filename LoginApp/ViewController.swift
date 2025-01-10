48058bcb78daa637406b49d0f1a53824eb815d9f
Jonathan Reátegui <j.reategui2012@gmail.com>
2025-01-10 10:21:26 -0500

    Adding wallpaper and logo images

--- LoginApp/ViewController.swift
+++ LoginApp/ViewController.swift
@@ -8,12 +8,33 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let wallpaperImageView: UIImageView = {
+        let imageView = UIImageView(frame: UIScreen.main.bounds)
+        imageView.image = UIImage(named: "wallpaper")
+        imageView.contentMode = .scaleAspectFill
+        return imageView
+    }()
+    
+    private let logoImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        self.view.insertSubview(wallpaperImageView, at: 0)
+        self.view.addSubview(logoImageView)
+        
+        NSLayoutConstraint.activate([
+            logoImageView.topAnchor.constraint(equalTo: self.view.safeAreaLayoutGuide.topAnchor, constant: 20),
+            logoImageView.centerXAnchor.constraint(equalTo: self.view.centerXAnchor),
+            logoImageView.widthAnchor.constraint(equalToConstant: 140),
+            logoImageView.heightAnchor.constraint(equalToConstant: 140)
+        ])
     }
-
-
 }
-
